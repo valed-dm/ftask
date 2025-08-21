d7f3cf251124ddb00cb78512eaa94aef7e05e78b
dmvaled <dmvaled@gmail.com>
2025-08-22 01:45:25 +0300

    refactor: logger got a new app name

--- app/core/logging.py
+++ app/core/logging.py
@@ -10,7 +10,7 @@ from .config import settings
 
 
 # Bound logger with a custom name available in record["extra"]
-log = logger.bind(name="SCOPED_AUTH")
+log = logger.bind(name="FTASK")
 
 log.add(
     settings.LOG_FILE,
